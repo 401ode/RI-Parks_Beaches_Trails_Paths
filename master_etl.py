383e47e1a9423f2f33ef8c51b9b73a59e30b5ae0
Brian J. McGuirk <brian.mcguirk@ode.ri.gov>
2016-07-07 17:42:12 -0400

    Addresses Issues #6, #10, #14, #9.

--- master_etl.py
+++ master_etl.py
@@ -4,6 +4,15 @@ from bs4 import BeautifulSoup
 import requests
 
 
+def replace_boolean_values(series):
+    """
+    A convenience DRY method to accommodate the needs of Google Sheets/JavaScript. 
+    """
+    series = series.replace(False, "N")
+    series = series.replace(True, "F")
+    return series
+
+
 def load_master():
     scorp_master = r"State Comprehensive Outdoor Recreation Plan Inventory of Facilities\SCORP_FILTER_GEO.xlsx"
     sg = pd.read_excel(scorp_master)
@@ -12,11 +21,12 @@ def load_master():
 
 def calc_swimming(df):
 	"""
+    GitHub Issue #6
     If pool or beach == 1, 'Swimming' = Y.
     """
+    
     df["swimming"] = df[["Pool","Beach"]] == 1
-    df["swimming"] = df["swimming"].replace(False,"N")
-    df["swimming"] = df["swimming"].replace(True,"Y")
+    df["swimming"] = replace_boolean_values(df["swimming"])
     return df
 
 
@@ -25,26 +35,31 @@ def filter_inappropriate_sites(df):
 	If owner == STA and GoRI_TestFlag == (Nan or 0) , filter out.
 
 	"""
-    df = ~df[df['OwnerTyp'].str.contains('STA') & df['GoRI_TestFlag'] == nan & df['GoRI_TestFlag'] == nan] # This is a hideous way to do this. I'll come up with a better one soon.
-    df = df[df['GoRI_TestFlag'] != 0]
+    df = ~df[df['OwnerTyp'].str.contains('STA') & df['GoRI_TestFlag'] == nan & df['GoRI_TestFlag'] == nan] # This is a hideous way to do this. 
+    # I'll come up with a better one soon.
+    df = df[df['GoRI_TestFlag'] != 0] 
     return df
 
 def calc_hiking(df):
 	"""
+    GitHub Issue #10
 	If 'Trails' or 'Walking Path' == 1, 'Hiking' = Y
     Boolean converted to Y/N for GoogleSheets/Javascript purposes.
 	"""
 
     df["Hiking"] = df[["Trails","Walking Path"]] == 1
-    df["Hiking"] = df["Hiking"].replace(False,"N")
-    df["Hiking"] = df["Hiking"].replace(True,"Y")
+    df["Hiking"] = replace_boolean_values(df["Hiking"])
     return df
 
 def calc_walking(df):
 	"""
+    GitHub Issue #14
 	If 'Track' or Walking Path' == 1, Walking = Y
 
 	"""
+    df["Walking"] = df[["Track","Walking Path","Trails"]] == 1
+    df["Walking"] = replace_boolean_values(df["Walking"])
+    return df
 
 
 def calc_running(df):
@@ -62,6 +77,13 @@ def calc_running(df):
 
     """
     df["Running"] = df[["Walking Path", "Track", "Bike Path", "Trails"]] == 1
-    df["Running"] = df["Running"].replace(False,"N")
-    df["Running"] = df["Running"].replace(True,"Y")
+    df["Running"] replace_boolean_values(df["Running"])
     return df
+
+def filter_all_n(df):
+    """
+    GitHub Issue #15
+    Using calculated fields of Running, Swimming, Biking, Paddling, Hiking, Walking and "Type" State Park. Filter list of assets.
+
+    """
+    
